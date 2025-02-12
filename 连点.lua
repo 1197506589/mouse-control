560649d02cdedea386142d6e785788f0438c8933
1197506589 <96377298+1197506589@users.noreply.github.com>
2025-02-13 04:22:43 +0800

    连点.lua

---
+++ 连点.lua
@@ -0,0 +1,44 @@
+local toggle = false
+local rapidFire = false
+
+-- 定义鼠标按键常量
+local mouseButton5 = 6
+local mouseLeft = 1
+local mouseRight = 3
+
+-- 启用鼠标事件报告
+EnablePrimaryMouseButtonEvents(true)
+
+-- 主循环
+function OnEvent(event, arg)
+    -- 检测鼠标5键按下
+    if event == "MOUSE_BUTTON_PRESSED" and arg == mouseButton5 then
+        toggle = not toggle  -- 切换模式
+        OutputLogMessage("Toggle mode: %s\n", toggle and "ON" or "OFF")
+    end
+
+    -- 如果模式开启
+    if toggle then
+        -- 检测鼠标左键按下
+        if event == "MOUSE_BUTTON_PRESSED" and arg == mouseLeft then
+            rapidFire = true
+            OutputLogMessage("Rapid fire started\n")
+        end
+
+        -- 检测鼠标左键释放
+        if event == "MOUSE_BUTTON_RELEASED" and arg == mouseLeft then
+            rapidFire = false
+            OutputLogMessage("Rapid fire stopped\n")
+        end
+
+        -- 如果连点模式开启
+        if rapidFire then
+          repeat
+            Sleep(50)  
+            PressMouseButton(mouseRight)
+            Sleep(5)  
+            ReleaseMouseButton(mouseRight)
+          until not IsMouseButtonPressed(1)
+        end
+    end
+end
